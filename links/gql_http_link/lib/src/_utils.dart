f7641623b019cc1f05a2ecc26100dacd9c2bebf5
Julien Zarka <julien.zarka@mobile-devices.fr>
2021-03-30 21:05:01 +0200

    Adapt JZ

--- links/gql_http_link/lib/src/_utils.dart
+++ links/gql_http_link/lib/src/_utils.dart
@@ -1,16 +1,12 @@
 import "dart:convert";
-import "dart:typed_data";
-import "package:meta/meta.dart";
 
-import "package:http/http.dart";
 import "package:gql/ast.dart";
 import "package:gql_exec/gql_exec.dart" as gql;
+import "package:http/http.dart";
 
 extension WithType on gql.Request {
   OperationType get type {
-    final definitions = operation.document.definitions
-        .whereType<OperationDefinitionNode>()
-        .toList();
+    final definitions = operation.document.definitions.whereType<OperationDefinitionNode>().toList();
     if (operation.operationName != null) {
       definitions.removeWhere(
         (node) => node.name.value != operation.operationName,
@@ -74,13 +70,43 @@ Map<String, MultipartFile> extractFlattenedFileMap(
   return currentMap;
 }
 
+/// According Elixir Absinthe specs https://hexdocs.pm/absinthe/file-uploads.html
+extension AddAllFilesAbsinthe on MultipartRequest {
+  void addAllFilesAbsinthe(Map<String, MultipartFile> fileMap) {
+    final List<MultipartFile> fileList = <MultipartFile>[];
+
+    final List<MapEntry<String, MultipartFile>> fileMapEntries = fileMap.entries.toList(growable: false);
+
+    for (int i = 0; i < fileMapEntries.length; i++) {
+      final MapEntry<String, MultipartFile> entry = fileMapEntries[i];
+      final MultipartFile f = entry.value;
+      fileList.add(MultipartFile(
+        f.field,
+        f.finalize(),
+        f.length,
+        contentType: f.contentType,
+        filename: f.filename,
+      ));
+    }
+    files.addAll(fileList);
+  }
+
+  set variableAbsinthe(String variables) {
+    if (variables != null) {
+      fields["variables"] = variables;
+    }
+  }
+
+  set queryAbsinthe(String body) => fields["query"] = body;
+}
+
+/// According Apollo specs https://github.com/jaydenseric/graphql-multipart-request-spec
 extension AddAllFiles on MultipartRequest {
   void addAllFiles(Map<String, MultipartFile> fileMap) {
     final Map<String, List<String>> fileMapping = <String, List<String>>{};
     final List<MultipartFile> fileList = <MultipartFile>[];
 
-    final List<MapEntry<String, MultipartFile>> fileMapEntries =
-        fileMap.entries.toList(growable: false);
+    final List<MapEntry<String, MultipartFile>> fileMapEntries = fileMap.entries.toList(growable: false);
 
     for (int i = 0; i < fileMapEntries.length; i++) {
       final MapEntry<String, MultipartFile> entry = fileMapEntries[i];
